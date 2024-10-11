87b3b19e49f297aeac4c74b8bb0ddb2be85d74fc
lollocenne <lorenzo.cennerilli@gmail.com>
2024-10-11 22:10:17 +0200

    Made the recognition for a list of string instead of a single string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class AutomaRiconoscitore:
-    def __init__(self, sequenza: str = "", caratteri: list[str] = []):
-        self.sequenza = sequenza
+    def __init__(self, sequenze: list[str] = [], caratteri: list[str] = []):
+        self.sequenze = sequenze
         self.caratteri = caratteri
         
         #ogni nodo è un dizionario dove la chiave è la sequenza che
@@ -12,38 +12,40 @@ class AutomaRiconoscitore:
         self.nodi: dict[str : dict[str : dict]] = {}
     
     #crea e collega i nodi, conta solo i caratteri che fanno parte della sequenza giusta,
-    def creaNodi(self):
+    def creaNodi(self, sequenza: str):
         self.nodi[""] = {}
-        for i in range(len(self.sequenza)-1, -1, -1):
-            if self.sequenza[i:] in self.nodi:
+        for i in range(len(sequenza)-1, -1, -1):
+            if sequenza[i:] in self.nodi:
                 continue
-            self.nodi[self.sequenza[i:]] = {self.sequenza[i]: self.sequenza[i+1:]}
+            self.nodi[sequenza[i:]] = {sequenza[i]: sequenza[i+1:]}
     
     #trova quale lettere se aggiunte a "parola" fanno "sequenza",
     #se non lo fanno allora la prima lettera di "parola" viene tolta,
     #se ancora non si trova ritorna la sequenza
-    def trovaCollegamento(self, parola: str) -> str:
+    def trovaCollegamento(self, sequenza: str, parola: str) -> str:
         lenParola = len(parola)
         while parola:
-            if parola + self.sequenza[lenParola:] in self.nodi:
-                return self.sequenza[lenParola:]
+            if parola + sequenza[lenParola:] in self.nodi:
+                return sequenza[lenParola:]
             parola = parola[1:]
             lenParola -= 1
-        return self.sequenza
+        return sequenza
     
     #finisce di collegare tutti i nodi
-    def collegaNodi(self):
+    def collegaNodi(self, sequenza: str):
         for key, value in self.nodi.items():
             for carattere in self.caratteri:
                 if carattere in value:
                     continue
-                value[carattere] = self.trovaCollegamento(self.sequenza[:len(key)] + carattere)
+                value[carattere] = self.trovaCollegamento(sequenza, sequenza[:len(key)] + carattere)
         
     #crea tutti i nodi con tutti i collegamenti
     def creaNodiAutoma(self):
-        self.__init__(self.sequenza, self.caratteri) #inizializza la classe
-        self.creaNodi() #crea i nodi
-        self.collegaNodi() #collega i nodi
+        self.__init__(self.sequenze, self.caratteri) #inizializza la classe
+        for seq in self.sequenze:
+            self.creaNodi(seq) #crea i nodi
+        for seq in self.sequenze:
+            self.collegaNodi(seq) #collega i nodi
     
     def __str__(self):
         return "\n".join([f"'{key}' : '{value}'" for key, value in self.nodi.items()])
@@ -51,6 +53,6 @@ class AutomaRiconoscitore:
 
 
 if __name__ == "__main__":
-    modello = AutomaRiconoscitore("ABAB", ["A", "B"])
+    modello = AutomaRiconoscitore(["ABA"], ["A", "B"])
     modello.creaNodiAutoma()
     print(modello)
